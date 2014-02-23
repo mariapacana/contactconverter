2260a72c8acf0ec155c6b3e6d37227633b986b94
Maria Pacana <maria.pacana@gmail.com>
2014-02-22 21:39:29 -0800

    * Includes missing phone type columns when source file is not in   Google format * Sets phone types (mobile, home, pager)

--- converter.rb
+++ converter.rb
@@ -4,15 +4,25 @@ require 'pry'
 
 class Contacts
 
+  MISSING_COLS = ['Phone 1 - Type', 'Phone 2 - Type', 'Phone 3 - Type', 'Phone 4 - Type']
+
   attr_accessor :data, :config
 
-  def initialize(source_file, config_file)
-    cardscan = lambda do |header|
+  def initialize(args)
+
+    change_headers = lambda do |header|
       header = @config[header].nil? ? header : @config[header]
     end
 
-    @config = YAML.load(File.open(config_file))
-    @data = CSV.read(source_file, headers: true, header_converters: cardscan)
+    @config = args[:config_file].nil? ? nil : YAML.load(File.open(args[:config_file]))
+
+    if args[:config_file].nil?
+      @data = CSV.read(args[:source_file], headers: true)
+    else
+      @data = CSV.read(args[:source_file], headers: true, header_converters: change_headers)
+    end
+
+    include_columns
 
   end
 
@@ -26,7 +36,7 @@ class Contacts
 
   def delete_blank_columns
     headers.each do |header|
-      if @data[header].uniq == [nil]
+      if @data[header].uniq.size < 3 && header != "Gender"
         @data.delete(header)
       end
     end
@@ -42,6 +52,27 @@ class Contacts
 
   def save_to_file(filename)
     File.open(filename, 'w') {|f| f.write(@data.to_s) }
+  end
+
+  def include_columns
+    MISSING_COLS. each { |col| @data[col] = nil }
+  end
+
+  def get_phone_types
+    @data.each do |person|
+      person['Phone 2 - Type'] = 'mobile' if person['Phone 2 - Value'] 
+      person['Phone 3 - Type'] = 'home' if person['Phone 3 - Value'] 
+      person['Phone 4 - Type'] = 'pager' if person['Phone 4 - Value'] 
+
+      if person['Phone 1 - Value'] == person['Phone 2 - Value']
+        person['Phone 1 - Type'] = 'mobile'
+      elsif person['Phone 1 - Value'] == person['Phone 3 - Value']
+        person['Phone 1 - Type'] = 'home'
+      elsif person['Phone 1 - Value'] == person['Phone 4 - Value']
+        person['Phone 1 - Type'] = 'pager'
+      end 
+
+    end
   end
 
 end
