4d8206d12851bb26b7650e82e072190f3431f954
Maria Pacana <maria.pacana@gmail.com>
2014-03-17 14:28:20 -0700

    * Created a header module to organize everything with Google headers.

---
+++ header.rb
@@ -0,0 +1,31 @@
+require_relative 'constants'
+require_relative 'util'
+
+module Header
+
+  include Constants
+  include Util
+
+  def self.non_google_headers(contacts)
+    contacts.headers - G_HEADERS
+  end
+
+  def self.updated_headers(contacts)
+    G_HEADERS - ["Notes"] + self.non_google_headers(contacts) + ["Notes"]
+  end
+
+  def self.add_missing_headers(updated_headers, contact)
+    updated_headers.map do |header|
+      !Util.nil_or_empty?(contact[header]) ? contact[header] : nil
+    end
+  end
+
+  def self.headers_in_order(contacts)
+    updated_headers = self.updated_headers(contacts)
+    new_contacts = contacts.map do |contact|
+      CSV::Row.new(updated_headers,
+                   self.add_missing_headers(updated_headers, contact))
+    end
+    CSV::Table.new(new_contacts)
+  end
+end
